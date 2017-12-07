a7935f6db70a0f9c20fb7ff8d1e2a6ec0d330d3c
Karl <karljellison@hotmail.com>
2017-12-07 16:20:16 +0000

    project

--- KarlEllison-405/Code/sketch.js
+++ KarlEllison-405/Code/sketch.js
@@ -8,7 +8,7 @@ function draw(){
   background(51);
   strokeWeight(10);
   translate(width/2,height-20);
-  branch(0); 
+  branch(0);
 }
 
 function branch(depth){
@@ -18,15 +18,15 @@ function branch(depth){
       translate(0,-height/10); // move the space upwards
       rotate(random(-0.05,0.05));  // random rotation
 
-      if (random(1.0) < 0.6){ // branching
-        rotate(0.3); // rotate branch to the right
+      if (random(1.0) < 0.66){ // branching
+        rotate(0.33); // rotate branch to the right
         scale(0.8); // downsize branch
 
         push(); // save the transform state
         branch(depth + 1); // Add another branch branch while depth <15
         pop(); // move back to saved state
 
-        rotate(-0.6); // rotate back to the left
+        rotate(-0.66); // rotate back to the left
 
         push(); // save state
         branch(depth + 1);   // start a second new branch while depth<15
@@ -38,3 +38,6 @@ function branch(depth){
     }
   }
 }
+
+
+// add class for branch? Animate starfield or fireworks? OnClick for fireworks. Moon arc
